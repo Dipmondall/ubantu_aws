26a1483f512d946c2b1299f32ff624620edc7b7b
dip <dip2020.mondal@gmail.com>
2022-11-05 20:30:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>hello world</h1>
+        <form>
+          <h2>exploit url path</h2>
+          <input className='exploit_url_path' name='exploit_url_path' />
+          <h2>exploit headers</h2>
+          <input className='' name='exploit_headers' />
+          <h2>exploit body</h2>
+          <input className='' name='exploit_body' />
+          <button type='submit'>submit</button>
+
+        </form>
       </header>
     </div>
   );
